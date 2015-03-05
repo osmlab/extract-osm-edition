d5e34c9a43380c36d6caad63ff65ec6790bb7a5f
Rub21 <ruben@mapbox.com>
2015-03-05 12:30:54 -0500

    good process

--- index.js
+++ index.js
@@ -1,42 +1,29 @@
 var fs = require('fs');
 var osmium = require('osmium');
 var argv = require('optimist').argv;
-var pg = require('pg');
 var _ = require('underscore');
-var users = require('./users');
 var osmfile = argv.osmfile;
 var boundfile = argv.boundfile;
 var path = argv.path;
 var nodes = {};
 var bounds;
-var client = new pg.Client(
-	"postgres://" + (argv.pguser || 'postgres') +
-	":" + (argv.pgpassword || '1234') +
-	"@" + (argv.pghost || 'localhost') +
-	"/" + (argv.pgdatabase || 'dbextract')
-);
-client.connect(function(err) {
-	if (err) {
-		return console.error('could not connect to postgres', err);
-	}
-});
+var geojson = {
+	"type": "FeatureCollection",
+	"features": []
+};
 fs.readFile(boundfile, 'utf8', function(err, data) {
 	if (err) {
 		console.log('Error: ' + err);
 		return;
 	}
 	bounds = JSON.parse(data);
-	console.log(bounds);
 	var file = new osmium.File(path + osmfile);
 	var reader = new osmium.Reader(file);
 	var handler = new osmium.Handler();
-
 	console.log('Process file : ' + osmfile);
-
 	handler.on('node', function(node) {
-		var coord = [node.lon, node.lat];		
+		var coord = [node.lon, node.lat];
 		if (pointinpolygon(coord, bounds.features[0].geometry.coordinates[0])) {
-			//console.log(node.user);
 			nodes[node.id] = coord;
 		}
 	});
@@ -49,35 +36,24 @@ fs.readFile(boundfile, 'utf8', function(err, data) {
 				"coordinates": []
 			}
 		};
-
-		console.log(way.user);
-		console.log(_.findIndex(users.users, {id: way.uid}));
-		if (_.findIndex(users.users, {id: way.uid}) > 0) {
-
-
-			console.log('=======================' + way.user + '=======================');
-			// for (var i = 0; i < way.nodes().length; i++) {
-			// 	if (nodes.hasOwnProperty(way.nodes()[i])) {
-			// 		feature.geometry.coordinates.push(nodes[way.nodes()[i]]);
-			// 	}
-			// }
-			// if (feature.geometry.coordinates.length > 0) {
-			// 	//geojson.features.push(feature);
-			// 	console.log(feature);
-			// 	// client.query(query_insert, function(err, result) {
-			// 	// 	if (err) {
-			// 	// 		console.log("error en insertar" + err);
-			// 	// 	}
-			// 	// });
-			// }
-
-
+		for (var i = 0; i < way.nodes().length; i++) {
+			if (nodes.hasOwnProperty(way.nodes()[i])) {
+				feature.geometry.coordinates.push(nodes[way.nodes()[i]]);
+			}
 		}
-
-
-
+		if (feature.geometry.coordinates.length > 0) {
+			feature.properties['osmuser'] = way.user;
+			feature.properties['osmtimestamp'] = way.timestamp;
+			geojson.features.push(feature);
+		}
 	});
 	reader.apply(handler);
+	var outputFilename = path + boundfile.split('.')[0] + '-' + osmfile.split('.')[0] + '.geojson';
+	fs.writeFile(outputFilename, JSON.stringify(geojson), function(err) {
+		if (err) {
+			console.log(err);
+		}
+	});
 });
 
 function pointinpolygon(point, vs) {
